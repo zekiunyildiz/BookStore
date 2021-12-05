65d78a0149eaa2b1cd3d0c4da4bf1278878b7f0e
zekiunyildiz <zekiunyildiz@gmail.com>
2021-12-06 00:20:35 +0300

    Put, Post and delete endpoint

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -48,5 +48,47 @@ namespace WebApi.Controllers
             var book = BookList.Where(book => book.Id == id).SingleOrDefault();
             return book;
         }
+
+        [HttpPost]
+        public IActionResult AddBook(Book newBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if (book is not null)
+                return BadRequest();
+
+            BookList.Add(newBook);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book updateBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+
+            if (book is null)
+            {
+                return BadRequest();
+            }
+
+            book.GenreId = updateBook.GenreId != default ? updateBook.GenreId : book.GenreId;
+            book.PageCount = updateBook.PageCount != default ? updateBook.PageCount : book.PageCount;
+            book.PublishDate = updateBook.PublishDate != default ? updateBook.PublishDate : book.PublishDate;
+            book.Title = updateBook.Title != default ? updateBook.Title : book.Title;
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book is null)
+            {
+                return BadRequest();
+            }
+            BookList.Remove(book);
+            return Ok();
+        }
+
     }
 }
